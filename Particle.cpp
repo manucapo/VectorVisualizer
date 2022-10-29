0ae114f1ff6039fb96bb11e3a6836cea0f35f45e
manucapo <manolotelleria@hotmail.com>
2022-10-29 19:03:00 +0200

    Clean up

--- Particle.cpp
+++ Particle.cpp
@@ -1,10 +1,3 @@
-//
-//  Particle.cpp
-//  SeminarProjectManu
-//
-//  Created by Manuel Telleria on 21.06.18.
-//  Copyright © 2018 Manuel Telleria. All rights reserved.
-//
 
 #include "Particle.hpp"
 
